3f66b8ba6c66fef5a09f12902f984e11f4256277
SINTA MERRIN JACOB <jacobsinta@gmail.com>
2021-01-22 15:32:21 +0530

    Update generate_tfrecord.py

--- tfrecord/generate_tfrecord.py
+++ tfrecord/generate_tfrecord.py
@@ -45,7 +45,7 @@ def split(df, group):
 
 
 def create_tf_example(group, path):
-    with tf.io.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
     image = Image.open(encoded_jpg_io)
@@ -86,7 +86,7 @@ def create_tf_example(group, path):
 
 
 def main(_):
-    writer = tf.io.TFRecordWriter(FLAGS.output_path)
+    writer = tf.python_io.TFRecordWriter (FLAGS.output_path)
     path = os.path.join(FLAGS.image_dir)
     examples = pd.read_csv(FLAGS.csv_input)
     grouped = split(examples, 'filename')
@@ -100,4 +100,4 @@ def main(_):
 
 
 if __name__ == '__main__':
-    tf.compat.v1.app.run()
+    tf.app.run()
